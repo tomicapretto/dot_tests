bdbc109ca54d094efdb8b30507e28e10b3b94129
Tomas Capretto <tomicapretto@gmail.com>
2021-12-02 19:28:22 -0300

    add more general method

--- example.c
+++ example.c
@@ -49,8 +49,7 @@ static PyObject *mat_vec_1d(PyObject *self, PyObject *args) {
 
     // This is where the _multiplication_ happens
     for (int i = 0; i < size; i++) {
-        int j = indices_data[i];
-        result_data[i] = other_data[j];
+        result_data[i] = other_data[indices_data[i]];
     }
 
     Py_DECREF(indices_arr);
@@ -60,6 +59,73 @@ static PyObject *mat_vec_1d(PyObject *self, PyObject *args) {
 fail:
     Py_DECREF(indices_arr);
     Py_DECREF(other_arr);
+    Py_XDECREF(result);
+    return NULL;
+}
+
+
+static PyObject *mat_vec_1d_dummy(PyObject *self, PyObject *args) {
+
+    // Inputs
+    PyObject *matrix; // 1d np.array
+    PyObject *vector;   // 1d np.array
+    PyObject *result;  // 1d np.array
+
+    // Outputs (np.array)
+    PyObject *output_array = NULL; // 1d np.array
+
+    if (!PyArg_ParseTuple(args, "O!O!O!", &PyArray_Type, &matrix, &PyArray_Type, &vector, &PyArray_Type, &result)) {
+        return NULL;
+    }
+
+    PyArrayObject *matrix_arr;
+    PyArrayObject *vector_arr;
+
+    matrix_arr = PyArray_FROM_OTF(matrix, NPY_INT, NPY_ARRAY_IN_ARRAY);
+    if (matrix_arr == NULL) {
+        Py_XDECREF(matrix_arr);
+        return NULL;
+    }
+
+    vector_arr = PyArray_FROM_OTF(vector, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+    if (vector_arr == NULL) {
+        Py_XDECREF(vector_arr);
+        return NULL;
+    }
+
+    PyObject *result_arr = NULL;
+
+#if NPY_API_VERSION >= 0x0000000c
+    result_arr = PyArray_FROM_OTF(result, NPY_DOUBLE, NPY_ARRAY_INOUT_ARRAY2);
+#else
+    result_arr = PyArray_FROM_OTF(result, NPY_DOUBLE, NPY_ARRAY_INOUT_ARRAY);
+#endif
+
+    int size = PyArray_SIZE(matrix_arr);
+
+    int *matrix_data = PyArray_DATA(matrix_arr);
+    double *vector_data = PyArray_DATA(vector_arr);
+    double *result_data = PyArray_DATA(result_arr);
+
+    // This is where the _multiplication_ happens
+    for (int i = 0; i < size; i += 2) {
+        result_data[matrix_data[i]] += vector_data[matrix_data[i + 1]];
+    }
+    /*
+    for (int i=0; i<n; ++i) {
+        y[i] = 0.0;
+        for (int j = offset_data[i]; j < offset_data[i + 1]; ++j) {
+            result_data[i] += vector_data[j] * x[col[j]];
+        }
+    }
+    */
+
+    Py_DECREF(matrix_arr);
+    Py_DECREF(vector_arr);
+    return result;
+fail:
+    Py_DECREF(matrix_arr);
+    Py_DECREF(vector_arr);
     Py_XDECREF(result);
     return NULL;
 }
@@ -90,6 +156,7 @@ static PyObject * example_wrapper(PyObject *self, PyObject *args) {
 
     size = PyArray_SIZE(arr1);
     dptr = PyArray_DATA(arr1);
+
 
     while (size--) {
         dptr++;
@@ -128,6 +195,7 @@ static PyObject * example_wrapper(PyObject *self, PyObject *args) {
 static PyMethodDef ModuleMethods[] = {
     {"example", example_wrapper, METH_VARARGS, "This is my example wrapper"},
     {"mat_vec_1d", mat_vec_1d, METH_VARARGS, "This is mat dot vec 1d"},
+    {"mat_vec_1d_dummy", mat_vec_1d_dummy, METH_VARARGS, "bla bla bla"},
     {NULL, NULL, 0, NULL}
 };
 
